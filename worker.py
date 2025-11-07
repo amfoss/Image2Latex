21f985cab6eb4aea7486da0b5bc0cf933af87485
Nova-Stark <nova2023ai@gmail.com>
2025-11-07 10:49:58 +0000

    dev/optimized_version

--- worker.py
+++ worker.py
@@ -1,4 +1,9 @@
 import os 
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
 from dotenv import load_dotenv 
 from PIL import Image
 from pix2tex.cli import LatexOCR
